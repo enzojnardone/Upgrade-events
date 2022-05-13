2df193f5edbcaf56e843e85e97f2d856182ff4bd
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-05-13 17:42:07 +0200

    JS events iteración 2

--- events.js
+++ events.js
@@ -3,14 +3,30 @@ console.log("JS Events");
 // 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el
 // evento click que ejecute un console log con la información del evento del click
 
-const countClick = document.querySelector("#btnToClick");
-countClick.addEventListener('click', function (event) {
-  console.log(`Hemos hecho click`);
-});
+function eventClick(event) {
+  console.log(event);
+};
+
+document.getElementById('#btnToClick')
+addEventListener('click', eventClick)
 
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
-let input = document.getElementById('focus')
-inpunt.addEventListener('focus')
+let eventFocus = function () {
+  console.log(input.value);
+}
+
+const input = document.querySelector('input[class="focus"]');
+input.addEventListener("focus", eventFocus);
 
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+
+const input2 = document.querySelector('input');
+const log = document.getElementById('value');
+
+function updateValue() {
+  console.log(input2.value);
+};
+
+input2.addEventListener('input', updateValue);
+
